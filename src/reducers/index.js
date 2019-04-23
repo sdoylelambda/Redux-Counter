af384d89bf04ecf79449d3568f5355d7c1672602
Sean Doyle <sdoylelambda@gmail.com>
2019-04-23 16:38:32 -0400

    App wrapped in provider.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,14 +13,18 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
-    return ++state ;
-    // return {
-    //   ...state,
-    //   initialState: action.payload
-    // }
+    console.log(state.count)
+    return {
+      ...state,
+      count: state.count + 1 
+    }
+
     case DECREMENT:
     // Fill in the body of this case
-    return --state;
+    return {
+      ...state,
+      count: state.count - 1
+    }
 
     default:
       return state;
